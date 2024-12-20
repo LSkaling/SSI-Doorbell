8b24208806e44f90b05a8eff0d8016690eabe0b6
Lawton Skaling <lskaling@icloud.com>
2024-12-20 14:08:11 -0900

    changed flask app for deployment

--- publisher_server.py
+++ publisher_server.py
@@ -5,25 +5,28 @@ from publisher.scheduler import start_scheduler
 from publisher.database import initialize_database
 from publisher.routes import setup_routes
 
+
+# Initialize Flask and Slack apps
+flask_app, slack_app, handler = create_app()
+
+setup_routes(flask_app, handler)
+
+# Initialize Database
+initialize_database()
+
+# Setup MQTT
+mqtt_client, node_states = setup_mqtt(slack_app)
+
+# Setup Slack commands
+#slack_channel = "C7F3VVB1S" #test
+slack_channel = "C03RXCV2FP0" #workspace-core
+setup_slack_commands(slack_app, mqtt_client, slack_channel)
+
+# Start scheduler
+start_scheduler(slack_app, slack_channel)
+
+app = flask_app
+
 if __name__ == "__main__":
-    # Initialize Flask and Slack apps
-    flask_app, slack_app, handler = create_app()
-
-    setup_routes(flask_app, handler)
-
-    # Initialize Database
-    initialize_database()
-
-    # Setup MQTT
-    mqtt_client, node_states = setup_mqtt(slack_app)
-
-    # Setup Slack commands
-    slack_channel = "C7F3VVB1S" #test
-    slack_channel = "C03RXCV2FP0" #workspace-core
-    setup_slack_commands(slack_app, mqtt_client, slack_channel)
-
-    # Start scheduler
-    start_scheduler(slack_app, slack_channel)
-
     # Start Flask app
     flask_app.run(port=3000)
